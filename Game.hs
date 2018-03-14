0b24749cf43c9373b8d1fe576d609ba6bf18f513
Mikael Janhonen <janhon3n@gmail.com>
2018-03-14 22:05:23 +0200

    Finish main game code

--- Game.hs
+++ Game.hs
@@ -1,5 +1,13 @@
 import GameState
+import Data.List (maximumBy)
+import Data.Function (on)
 import System.Random
+
+deleteElementAtIndex :: Int -> [a] -> [a]
+deleteElementAtIndex _ []     = []
+deleteElementAtIndex i (a:as)
+  | i == 0    = as
+  | otherwise = a : deleteElementAtIndex (i-1) as
 
 initPlayerState :: Int -> StdGen -> (PlayerState, StdGen)
 initPlayerState i g = do
@@ -16,15 +24,18 @@ initPlayerStates n i g = do
 
 initGameState :: StdGen -> (GameState, StdGen)
 initGameState g = do
-      let (playerStates, g') = initPlayerStates 2 1 g
-      (GameState {turn=1, playerStates=playerStates}, g')
+   let (playerStates, g') = initPlayerStates 2 1 g
+   (GameState {turn=1, playerStates=playerStates}, g')
 
 
-chooseFunction :: PlayerState -> (PlayerState, Function)
-chooseFunction (PlayerState {index=index, score=score, hand=hand}) = do
-   let function = head hand
-   let newHand = tail hand
-   (PlayerState {index=index, score=score, hand=newHand}, function)
+getFunction :: PlayerState -> Int -> (PlayerState, Function)
+getFunction playerState@(PlayerState {index=index, score=score, hand=hand}) i = do
+   if i > length hand
+      then error ("Invlid index " ++ (show i))
+   else do
+      let function = hand !! i
+      let newHand = deleteElementAtIndex i hand
+      (PlayerState {index=index, score=score, hand=newHand}, function)
 
 executeFunctions :: PlayerState -> [Function] -> PlayerState 
 executeFunctions playerState functions = foldl executeFunction playerState functions
@@ -39,18 +50,33 @@ addRandomFunctionsToPlayerHands states g = do
       let (rest, g'') = addRandomFunctionsToPlayerHands (tail states) g'
       (newPlayerState:rest, g'')
 
+askUsersForInput :: Int -> Int -> IO [Int]
+askUsersForInput 0 _ = return []
+askUsersForInput n i = do
+   putStrLn $ "Player "++ (show i) ++ ", select your function:"
+   line <- getLine
+   let index = fromEnum (line !! 0) - 97
+   rest <- askUsersForInput (n-1) (i+1)
+   return (index :  rest)
+
+
 gameLoop :: GameState -> StdGen -> IO ()
-gameLoop (GameState {turn=4, playerStates=_}) _ = return ()
+gameLoop (GameState {turn=4, playerStates=playerStates}) _ = do
+   let scores = map score playerStates
+   let winner = maximumBy (compare `on` score) playerStates
+   putStrLn $ "The winner is player " ++ (show (index winner)) ++ " with a score of " ++ (show (score winner))
+   return ()
+
 gameLoop gameState@(GameState {turn=turn, playerStates=playerStates}) g = do
    putStrLn $ "Turn " ++ (show turn)
    print gameState
-   let (playerStates', chosenFunctions)  = unzip $ map chooseFunction playerStates
+   functionIndicies <- askUsersForInput (length playerStates) 1
+   let (playerStates', chosenFunctions)  = unzip $ map (\(x, y) -> getFunction x y) (zip playerStates functionIndicies)
    let playerStates'' = map (\s -> executeFunctions s chosenFunctions) playerStates'
    let (newPlayerStates, g') = addRandomFunctionsToPlayerHands playerStates'' g
    let newGameState = GameState {turn=(turn+1), playerStates=newPlayerStates}
    gameLoop newGameState g
 
-   
 
 main = do
    g <- newStdGen
