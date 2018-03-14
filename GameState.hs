d9863e5769b0efbfe8e9e904e86e61936294d375
Mikael Janhonen <janhon3n@gmail.com>
2018-03-14 22:14:41 +0200

    Add Exponentiation function

--- GameState.hs
+++ GameState.hs
@@ -1,7 +1,7 @@
 module GameState where
 import System.Random
 
-data Function = Addition Int | Multiplication Int | Division Int
+data Function = Addition Int | Multiplication Int | Division Int | Exponentiation Int
 
 class Exceutable a where
    execute :: a -> Int -> Int
@@ -10,6 +10,7 @@ instance Exceutable Function where
    execute (Addition i) = (+i)
    execute (Multiplication i) = (*i)
    execute (Division i) = flip quot i
+   execute (Exponentiation i) = flip quot i
 
 instance Show Function where
    show (Addition i)
@@ -21,7 +22,9 @@ instance Show Function where
    show (Division i)
       | i >= 0 = "x/" ++ (show i)
       | i < 0 = "x/(" ++ (show i) ++ ")"
-
+   show (Exponentiation i)
+      | i >= 0 = "x^" ++ (show i)
+      | i < 0 = "x^(" ++ (show i) ++ ")"
 data PlayerState = PlayerState {
    index :: Int,
    score :: Int,
@@ -44,12 +47,15 @@ instance Show GameState where
 
 randomFunction :: StdGen -> (Function, StdGen)
 randomFunction g = do
-   let (randomInteger, g') = randomR (0,999) g :: (Int, StdGen)
-   let (randomFunctionIndex, g'') = randomR (0,2) g' :: (Int, StdGen)
+   let (smallInt, g1) = randomR (0,10) g :: (Int, StdGen)
+   let (mediumInt, g2) = randomR (0,100) g1 :: (Int, StdGen)
+   let (largeInt, g3) = randomR (0,1000) g2 :: (Int, StdGen)
+   let (randomFunctionIndex, g4) = randomR (0,3) g3 :: (Int, StdGen)
    case randomFunctionIndex of
-      0 -> (Addition randomInteger, g'')
-      1 -> (Multiplication randomInteger, g'')
-      2 -> (Division randomInteger, g'')
+      0 -> (Addition largeInt, g4)
+      1 -> (Multiplication mediumInt, g4)
+      2 -> (Division mediumInt, g4)
+      3 -> (Exponentiation smallInt, g4)
 
 randomHand :: Int -> StdGen -> ([Function], StdGen)
 randomHand 0 g = ([], g)
