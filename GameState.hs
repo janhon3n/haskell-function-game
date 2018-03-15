6b9aef9806691a9d87f55a3ebc7690ea71714190
Mikael Janhonen <janhon3n@gmail.com>
2018-03-15 09:29:44 +0200

    Edit random integer intervals in randomFunction creation

--- GameState.hs
+++ GameState.hs
@@ -48,8 +48,8 @@ instance Show GameState where
 randomFunction :: StdGen -> (Function, StdGen)
 randomFunction g = do
    let (smallInt, g1) = randomR (0,10) g :: (Int, StdGen)
-   let (mediumInt, g2) = randomR (0,100) g1 :: (Int, StdGen)
-   let (largeInt, g3) = randomR (0,1000) g2 :: (Int, StdGen)
+   let (mediumInt, g2) = randomR (-100,100) g1 :: (Int, StdGen)
+   let (largeInt, g3) = randomR (-1000,1000) g2 :: (Int, StdGen)
    let (randomFunctionIndex, g4) = randomR (0,3) g3 :: (Int, StdGen)
    case randomFunctionIndex of
       0 -> (Addition largeInt, g4)
